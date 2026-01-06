4869d1af48646f4fcbaff3c364ae82e660d844c6
GiaBao <duonggiabao254@gmail.com>
2026-01-07 00:37:02 +0700

    feat: implement theme provider and update button usage example

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,17 @@
 import Button from "@mui/material/Button";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import Typography from "@mui/material/Typography";
 
 export default function ButtonUsage() {
   return (
     <>
-      <Button variant='contained'>Hello world</Button>
+      <Typography variant='body2' color='textSecondary'>
+        MUI Button with Icons Example
+      </Typography>
+      <Button variant='contained' color='success'>
+        Hello world
+      </Button>
       <AccessAlarmIcon />
       <ThreeDRotation />
     </>
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme.ts";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -10,7 +12,9 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
