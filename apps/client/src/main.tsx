861d6048cdb0992b7bf925ececa7a1c23952f82b
GiaBao <duonggiabao254@gmail.com>
2026-01-07 00:12:22 +0700

    chore: remove unused CSS files and integrate CssBaseline in main.tsx

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -10,6 +10,7 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 );
