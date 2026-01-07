a487667b78429d5b949725b0362a639502dc3a5f
GiaBao <duonggiabao254@gmail.com>
2026-01-08 00:45:01 +0700

    feat: implement app layout with AppBar, BoardBar, and BoardContent components

---
+++ apps/client/src/pages/Boards/_id.tsx
@@ -0,0 +1,15 @@
+import AppBar from "@/components/AppBar";
+import BoardBar from "@/pages/Boards/BoardBar";
+import BoardContent from "@/pages/Boards/BoardContent";
+import Container from "@mui/material/Container";
+
+function Board() {
+  return (
+    <Container sx={{ height: "100vh", backgroundColor: "primary.main" }} disableGutters maxWidth={false}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  );
+}
+export default Board;
