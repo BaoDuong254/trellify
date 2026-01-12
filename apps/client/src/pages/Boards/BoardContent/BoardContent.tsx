9770ec70954a5bdaf807bc6e0a5a126c00e339aa
GiaBao <duonggiabao254@gmail.com>
2026-01-12 21:38:23 +0700

    fix: update card insertion logic to maintain order during drag-and-drop

--- apps/client/src/pages/Boards/BoardContent/BoardContent.tsx
+++ apps/client/src/pages/Boards/BoardContent/BoardContent.tsx
@@ -103,7 +103,11 @@ function BoardContent({ board }: { board: Board }) {
           ...(activeDraggingCardData as Card),
           columnId: nextOverColumn._id,
         };
-        nextOverColumn.cards = nextOverColumn.cards.splice(newCardIndex, 0, rebuildActiveDraggingCardData);
+        nextOverColumn.cards = [
+          ...nextOverColumn.cards.slice(0, newCardIndex),
+          rebuildActiveDraggingCardData,
+          ...nextOverColumn.cards.slice(newCardIndex),
+        ];
         // Remove placeholder card if exists
         nextOverColumn.cards = nextOverColumn.cards.filter((card) => !card.FE_PlaceholderCard);
         // Update card order ids
