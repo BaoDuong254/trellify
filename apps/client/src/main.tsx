4f2774c740001db59ee97d866646029336906f66
GiaBao <duonggiabao254@gmail.com>
2026-01-22 15:29:01 +0700

    feat: integrate Redux for active board management and add necessary dependencies

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "src/theme.ts";
 import { ToastContainer } from "react-toastify";
 import { ConfirmProvider } from "material-ui-confirm";
+import { Provider } from "react-redux";
+import { store } from "src/redux/store.ts";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -14,20 +16,22 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <ConfirmProvider
-        defaultOptions={{
-          allowClose: false,
-          dialogProps: { maxWidth: "xs" },
-          buttonOrder: ["confirm", "cancel"],
-          cancellationButtonProps: { color: "inherit" },
-          confirmationButtonProps: { color: "secondary", variant: "outlined" },
-        }}
-      >
-        <CssBaseline />
-        <App />
-        <ToastContainer position='bottom-left' theme='colored' />
-      </ConfirmProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ConfirmProvider
+          defaultOptions={{
+            allowClose: false,
+            dialogProps: { maxWidth: "xs" },
+            buttonOrder: ["confirm", "cancel"],
+            cancellationButtonProps: { color: "inherit" },
+            confirmationButtonProps: { color: "secondary", variant: "outlined" },
+          }}
+        >
+          <CssBaseline />
+          <App />
+          <ToastContainer position='bottom-left' theme='colored' />
+        </ConfirmProvider>
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
---
+++ apps/client/src/redux/activeBoard/activeBoardSlice.ts
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { isEmpty } from "lodash";
+import envConfig from "src/config/env";
+import type { Board } from "src/types/board.type";
+import { generatePlaceholderCard } from "src/utils/formatters";
+import { mapOrder } from "src/utils/sort";
+
+export interface ActiveBoardState {
+  currentActiveBoard: Board | null;
+}
+
+const initialState: ActiveBoardState = {
+  currentActiveBoard: null,
+};
+
+export const fetchBoardDetailsAPI = createAsyncThunk("activeBoard/fetchBoardDetailsAPI", async (boardId: string) => {
+  const response = await axios.get(`${envConfig.VITE_API_ENDPOINT}/v1/boards/${boardId}`);
+  return response.data.data;
+});
+
+export const activeBoardSlice = createSlice({
+  name: "activeBoard",
+  initialState,
+  reducers: {
+    updateCurrentActiveBoard: (state, action) => {
+      state.currentActiveBoard = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBoardDetailsAPI.fulfilled, (state, action) => {
+      const board = action.payload as Board;
+      board.columns = mapOrder(board.columns, board.columnOrderIds, "_id");
+
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)];
+          column.cardOrderIds = [generatePlaceholderCard(column)._id];
+        } else {
+          column.cards = mapOrder(column.cards, column.cardOrderIds, "_id");
+        }
+      });
+      state.currentActiveBoard = board;
+    });
+  },
+});
+
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions;
+
+export const selectCurrentActiveBoard = (state: { activeBoard: ActiveBoardState }) => {
+  return state.activeBoard.currentActiveBoard;
+};
+
+export const activeBoardReducer = activeBoardSlice.reducer;
